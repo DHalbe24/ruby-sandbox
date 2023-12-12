9c59be58279997611b9383ff44e355ba2e181b4c
DHalbe24 <137205368+DHalbe24@users.noreply.github.com>
2023-12-12 15:10:08 +0000

    Hello, world!

--- howdy.rb
+++ howdy.rb
@@ -1,12 +1,3 @@
-require "./goodbye.rb"
-require "active_support/all"
-require "awesome_print"
-require "pry-byebug"
-
-#my_string = "Howdy there!"
-
-#pp my_string
-
-x = 3
-
-pp x.ordinalize
+# here's a comment
+my_string = "Hello, world!"
+pp my_string
